3b2ce8c2f087acdca9885145cf158ca1f7af47a7
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-04-20 18:51:25 +0200

    installed react

---
+++ components/Footer/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/Beurre-moutarde"
+					target="_blank"
+				>
+					<img
+						src={require("../../assets/logos/github-logo.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.linkedin.com/in/christopher-lebreton-033a7720a/"
+					target="_blank"
+				>
+					<img
+						src={require("../../assets/logos/linkedin-logo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://twitter.com/Archer5263"
+					target="_blank"
+				>
+					<img
+						src={require("../../assets/logos/twitter-logo.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
